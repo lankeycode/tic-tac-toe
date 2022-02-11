5df10e3608107ad9395a176ea9f98c1221bd0147
lankeycode <michael.moussaoui@gmail.com>
2022-02-11 10:42:30 +0100

    Add .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './index.html',
+    './main.js',
+  ],
+  theme: {
+    fontFamily: {
+      'Pacifico': ['Pacifico', 'cursive'],
+      'Marker': ['Permanent Marker', 'cursive'],
+      'Kaushan': ['Kaushan Script', 'cursive'],
+      'Roboto': ['Roboto Slab', 'serif'],
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+// font - family: 'Kaushan Script', 'cursive';
+// font - family: 'Roboto Slab', 'serif';
