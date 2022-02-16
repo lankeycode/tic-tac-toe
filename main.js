5e37fb4fdc18ad69e5113928111e969ece29aa44
lankeycode <michael.moussaoui@gmail.com>
2022-02-16 16:40:01 +0100

    # 4 modified

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
 // Déclaration des constantes
-const mouse =document.querySelector('.mouse')
-const containerImgOne = document.querySelector('.containerImgOne')
-const containerImgTwo = document.querySelector('.containerImgTwo')
 const player1 = document.querySelector('.player1')
 const player2 = document.querySelector('.player2')
 const circle1 = document.querySelector('.circle1')
 const circle2 = document.querySelector('.circle2')
 const square = document.querySelectorAll('.square')
-const bigImg = document.querySelector('.bigImg')
-const smallImg = document.querySelector('.smallImg')
-const start = document.querySelector('.start')
-const reset = document.querySelector('.reset')
+const start = document.querySelector('#start')
+// const reset = document.querySelector('.reset')
+
+ $buttons = $("button[id*='button_']")
 
 // Déclaration des variables
-let number1 = ''  
-let number2 = '' 
-let number3 = ''
-let number4 = ''
-let number5 = ''
-let number6 = ''
-let number7 = ''
-let number8 = ''
-let number9 = ''
-let number = ''
+let n1 = document.querySelector('.n1')
+let n2 = document.querySelector('.n2')
+let n3 = document.querySelector('.n3')
+let n4 = document.querySelector('.n4')
+let n5 = document.querySelector('.n5')
+let n6 = document.querySelector('.n6')
+let n7 = document.querySelector('.n7')
+let n8 = document.querySelector('.n8')
+let n9 = document.querySelector('.n9')
 let playerActive = ''
 let playerLeft = document.querySelector('.playerLeft')
 let playerRight = document.querySelector('.playerRight')
 
-window.addEventListener('mousemove', (e) => {
-    mouse.style.left = e.pageX + 'px'
-    mouse.style.top = e.pageY + 'px'
-})
 
-// Choisir son theme de jeu
-
-
-// Savoir quel joueur commence
+// what is the player start
 function whoStartGame() {
     return Math.floor(Math.random() * 2) + 1
 }
@@ -50,7 +39,7 @@ function switchPlayer() {
      playerActive === 'player1' ? playerActive = 'player2' : playerActive = 'player1'
     indicatePlayer()
 }
-
+// Display active player
 function indicatePlayer(){
     if (playerActive === 'player1'){
     player1.classList.add('activePlayer')
@@ -71,187 +60,56 @@ function winner() {
     // 1 - 2 - 3  ||  4 - 5 - 6 ||  7 - 8 - 9 ||  1 - 5 - 9  || 3 - 5 - 7 
     // 1 - 4 - 7  ||  2 - 5 - 8 ||  3 - 6 - 9
     // alors le joueur actif gagne
-    if ((number1 === 'player1' && number2 === 'player1' && number3 === 'player1') || 
-     ( number4 === 'player1' && number5 === 'player1' && number6 === 'player1') ||
-     ( number7 === 'player1' && number8 === 'player1' && number9 === 'player1')||
-     ( number1 === 'player1' && number5 ==='player1' && number9 === 'player1')||
-     (number3 === 'player1' && number5 === 'player1' && number7 === 'player1')||
-     (number1 === 'player1' && number4 === 'player1' && number7 === 'player1')||
-     (number2 === 'player1' && number5 === 'player1' && number8 === 'player1')||
-     (number3 === 'player1' && number6 === 'player1' && number9 === 'player1')) {
+    
+    if ($(" div button:first-child img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(2) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(3) img:first-child" ).hasClass('imageNaruto') === true||
+          $(" div button:nth-child(4) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(5) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(6) img:first-child" ).hasClass('imageNaruto') === true||
+          $(" div button:nth-child(7) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(8) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:last-child img:first-child" ).hasClass('imageNaruto') === true ||
+          $(" div button:first-child img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(5) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:last-child img:first-child" ).hasClass('imageNaruto') === true ||
+          $(" div button:nth-child(3) img:first-child" ).hasClass('imageNaruto') === true &&  $(" div button:nth-child(5) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(7) img:first-child" ).hasClass('imageNaruto') === true ||
+          $(" div button:first-child img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(4) img:first-child" ).hasClass('imageNaruto') === true &&  $(" div button:nth-child(7) img:first-child" ).hasClass('imageNaruto') === true ||
+          $(" div button:nth-child(2) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(5) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(8) img:first-child" ).hasClass('imageNaruto') === true ||
+          $(" div button:nth-child(3) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:nth-child(6) img:first-child" ).hasClass('imageNaruto') === true && $(" div button:last-child img:first-child" ).hasClass('imageNaruto') === true ){
          playerLeft.textContent = 'WINNER'
-         playerRight.textContent = 'LOSER'    
+         playerRight.textContent = 'LOSER' 
+         let scoreLeft = $(".scoreLeft")
+         scoreLeft.html( parseInt(scoreLeft.html()) + 1 ) 
+         $buttons.prop('disabled', true) //disabled all buttons
         
-    } else if((number1 === 'player2' && number2 === 'player2' && number3 === 'player2') || 
-    ( number4 === 'player2' && number5 === 'player2' && number6 === 'player2') ||
-    ( number7 === 'player2' && number8 === 'player2' && number9 === 'player2')||
-    ( number1 === 'player2' && number5 ==='player2' && number9 === 'player2')||
-    (number3 === 'player2' && number5 === 'player2' && number7 === 'player2')||
-    (number1 === 'player2' && number4 === 'playe21' && number7 === 'player2')||
-    (number2 === 'player2' && number5 === 'player2' && number8 === 'player2')||
-    (number3 === 'player2' && number6 === 'player2' && number9 === 'player2')) {
+     }else if ($(" div button:first-child img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(2) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(3) img:first-child" ).hasClass('imagePiece') === true||
+     $(" div button:nth-child(4) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(5) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(6) img:first-child" ).hasClass('imagePiece') === true||
+     $(" div button:nth-child(7) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(8) img:first-child" ).hasClass('imagePiece') === true && $(" div button:last-child img:first-child" ).hasClass('imagePiece') === true ||
+     $(" div button:first-child img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(5) img:first-child" ).hasClass('imagePiece') === true && $(" div button:last-child img:first-child" ).hasClass('imagePiece') === true ||
+     $(" div button:nth-child(3) img:first-child" ).hasClass('imagePiece') === true &&  $(" div button:nth-child(5) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(7) img:first-child" ).hasClass('imagePiece') === true ||
+     $(" div button:first-child img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(4) img:first-child" ).hasClass('imagPieceo') === true &&  $(" div button:nth-child(7) img:first-child" ).hasClass('imagePiece') === true ||
+     $(" div button:nth-child(2) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(5) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(8) img:first-child" ).hasClass('imagePiece') === true ||
+     $(" div button:nth-child(3) img:first-child" ).hasClass('imagePiece') === true && $(" div button:nth-child(6) img:first-child" ).hasClass('imagePiece') === true && $(" div button:last-child img:first-child" ).hasClass('imagePiece') === true ){
         playerLeft.textContent = 'LOSER'
         playerRight.textContent = 'WINNER'
+        let scoreRight = $(".scoreRight")
+        scoreRight.html( parseInt(scoreRight.html()) + 1 )
+        $buttons.prop('disabled', true) //disabled all buttons
     }
+    newGame()
 }
-// empecher de cliquer 2 fois sur une meme case
-
-$.test1 = $(".n1").one("click",function()  { 
-     
-      if (playerActive === 'player1'){
-
-  $(".n1").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-  } else if (playerActive === 'player2') {
-
-  $(".n1").prepend('<img class="imageLogo" src="./img/one-piece.png" style="height: 120px;" alt="personnage naruto"></img>')
-}
-$(".n1").css("cursor" , "none")
-
- number1 = playerActive
-    indicatePlayer()
-    switchPlayer()
-    winner()   
-})
-
-$(".n2").one("click",function()  { 
-     
-    if (playerActive === 'player1'){
-
-    $(".n2").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-} else if (playerActive === 'player2') {
-    $(".n2").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-  }
-  $(".n2").css("cursor" , "none")  
+// display image of game
+$buttons.click(function ()  { 
   
-  number2 = playerActive     
-    indicatePlayer()
-    switchPlayer()
-    winner()     
-})
-
-$(".n3").one("click",function()  { 
-     
     if (playerActive === 'player1'){
-
-    $(".n3").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
+    $(this).prepend('<img class="imageNaruto imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto">1</img>')
 
 } else if (playerActive === 'player2') {
-    $(".n3").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-}
-$(".n3").css("cursor" , "none")
-
-number3 = playerActive
-    indicatePlayer()
-    switchPlayer()
-    winner()
-})
-
-$(".n4").one("click",function()  { 
-     
-    if (playerActive === 'player1'){
-
-    $(".n4").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-} else if (playerActive === 'player2') {
-    $(".n4").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
+    $(this).prepend('<img class="imagePiece imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto">2</img>')
   }
-  $(".n4").css("cursor" , "none") 
-  number4 = playerActive
-    
-    indicatePlayer()
-    switchPlayer()
-    winner()
-})
-
-$(".n5").one("click",function()  { 
-     
-    if (playerActive === 'player1'){
-
-    $(".n5").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-} else if (playerActive === 'player2') {
-    $(".n5").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-  }
-  $(".n5").css("cursor" , "none")
-  number5 = playerActive
-        
-    indicatePlayer()
-    switchPlayer()
-    winner()
-})
-
-$(".n6").one("click",function()  { 
-     
-    if (playerActive === 'player1'){
-
-    $(".n6").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-} else if (playerActive === 'player2') {
-    $(".n6").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-  }
-  $(".n6").css("cursor" , "none")
-   
-  number6 = playerActive
+  $(this).css("cursor" , "none")
+  $(".imageLogo").addClass("animate") 
   
     indicatePlayer()
     switchPlayer()
     winner()
-})
-
-$(".n7").one("click",function()  { 
-     
-    if (playerActive === 'player1'){
-
-    $(".n7").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-} else if (playerActive === 'player2') {
-    $(".n7").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-  }
-  $(".n7").css("cursor" , "none") 
-  number7 = playerActive
-     
-    indicatePlayer()
-    switchPlayer()
-    winner()
-})
-
-$(".n8").one("click",function()  { 
-     
-    if (playerActive === 'player1'){
-
-    $(".n8").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-} else if (playerActive === 'player2') {
-    $(".n8").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-  }
-  $(".n8").css("cursor" , "none")
-  number8 = playerActive
-  
-    indicatePlayer()
-    switchPlayer()
-    winner()
-})
-
-$(".n9").one ("click",function()  { 
-    if (playerActive === 'player1'){
-
-    $(".n9").prepend('<img class="imageLogo" src="./img/naruto-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-
-} else if (playerActive === 'player2') {
-    $(".n9").prepend('<img class="imageLogo" src="./img/one-piece-logo.png" style="height: 120px;" alt="personnage naruto"></img>')
-  }
-  $(".n9").css("cursor" , "none") 
-  number9 = playerActive
-
-    indicatePlayer()
-    switchPlayer()
-    winner()
+    $(this).prop('disabled', true) // disable cliked button 
 }) 
-
 // reset
-reset.addEventListener('click', () => {
+$("#reset").click( () => {
     player1.classList.remove('activePlayer')
     circle1.classList.remove('activeCircle')
     player2.classList.remove('activePlayer')
@@ -259,16 +117,10 @@ reset.addEventListener('click', () => {
     playerLeft.textContent = ''
     playerRight.textContent = ''
     $(".imageLogo").remove()
-    $(".square").css("cursor" , "pointer")  
-    location.reload()
-})
+    $(".square").css("cursor" , "pointer") 
+    $buttons.prop('disabled', false) //enable all buttons
+    })
 
-$(document).ready(function () {
-   
-})
 
-$("img").click(function() {
-    $(this).addClass("active2")
-  })
 
   
